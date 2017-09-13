f19c2bdf9a1306d86aea149aba66128203b374ec
Angelo Palmieri <palmieri.ang@gmail.com>
2017-09-13 10:45:21 +0200

    Lifting State Up

--- src/App.js
+++ src/App.js
@@ -2,25 +2,70 @@ import React, { Component } from 'react'
 import NewCard from './NewCard'
 import './App.css'
 
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+const suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+const deck = []
+let shuffledCards = []
+let card;
+
+function createDeck() {
+  for(var i = 0; i < cards.length; i++) {
+    for(var j = 0; j < suit.length; j++) {
+      deck.push({'value': cards[i], 'suit': suit[j], 'point': getPoints(cards[i])})
+    }
+  }
+  return deck
+}
+
+function getPoints(card) {
+  if(card === 'A') {
+    return 11
+  } else if(card === 'J' || card === 'Q' || card === 'K') {
+    return 10
+  } else {
+    return card
+  }
+}
+
+(function shuffle() {
+  shuffledCards = createDeck()
+  for (var i = shuffledCards.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = shuffledCards[i]
+    shuffledCards[i] = shuffledCards[j]
+    shuffledCards[j] = temp
+  }
+  return shuffledCards
+})()
+
+function deal() {
+  card = shuffledCards[0]
+  shuffledCards.splice(0, 1)
+}
+
+deal()
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      dealerCards: [1, 2],
-      playerCards: [1, 2]
+      value: card.value,
+      suit: card.suit,
+      point: card.point,
+      dealerCards: [1],
+      playerCards: [1]
     }
   }
 
   render() {
-    const {playerCards} = this.state
+    const {value, suit, playerCards} = this.state
     console.log(playerCards)
 
     return (
       <div className="App">
         <div className="player">
           {playerCards.map((card, index) => (
-            <NewCard key={index} />
+            <NewCard key={index} value={value} suit={suit} />
           ))}
         </div>
       </div>
